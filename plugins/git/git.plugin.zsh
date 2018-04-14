0efb82074965be1a57fb0dbfe8e9ee1035da5dd5
Rareș Cosma <rares@getbetter.ro>
2018-04-14 07:59:26 +0200

    Re-alias 'gs' to git status

--- plugins/git/git.plugin.zsh
+++ plugins/git/git.plugin.zsh
@@ -383,7 +383,7 @@ is-at-least 2.13 "$git_version" \
   && alias gsta='git stash push' \
   || alias gsta='git stash save'
 alias gsts='git stash show --patch'
-alias gst='git status'
+alias gs='git status'
 alias gss='git status --short'
 alias gsb='git status --short --branch'
 alias gsi='git submodule init'
